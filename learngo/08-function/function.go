b9a5a959d7a59fc1262db2c9445a105ece73a51d
wding <wding@nexient.com>
2020-07-07 00:57:43 -0400

    fibonacci

--- learngo/08-function/function.go
+++ learngo/08-function/function.go
@@ -8,6 +8,7 @@ import (
 func main() {
   parameters()
   closures()
+  fibonc()
 }
 
 func parameters() {
@@ -44,4 +45,26 @@ func adder() func(int) int {
 		sum += x
 		return sum
 	}
+}
+
+func fibonc(){
+  fmt.Println("-- Fibonacci closure")
+  
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Print(f(i),",")
+	}
+	fmt.Println()
+}
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func(int) int {
+  return func(n int) int {
+    x,y := 0,1
+	for i := 0; i < n; i++{
+	  x,y = y,x+y
+	}
+	return x
+  }  
 }
