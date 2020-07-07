d9bc5c6ec2da2d7f6a83abcdf1aa3cc33a127f5e
wding <wding@nexient.com>
2020-07-07 00:10:23 -0400

    update count

--- learngo/07-count/count.go
+++ learngo/07-count/count.go
@@ -12,14 +12,8 @@ func main() {
 func WordCount(s string) map[string]int {
   m := make(map[string]int)
   
-  var a []string = strings.Split(s," ")
-  for i := 0; i < len(a); i++{
-    _, ok := m[a[i]]
-	if ok {
-	  m[a[i]]++
-	} else { 
-      m[a[i]] = 1
-	}
-  }
+  words := strings.Split(s," ")
+  for _,v := range words { m[v]++ }
+  
   return m
 }
